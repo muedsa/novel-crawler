fd4f07ef6e20405c71d0800b270495eda2817250
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-02-07 17:57:24 +0800

    fix: buildMetrics

--- src/utils.ts
+++ src/utils.ts
@@ -28,40 +28,41 @@ const buildMetrics = (statistic: NovelCrawlerStatistic) => {
 
     metrics += "# HELP crawler_start_at 开始时间(Timestamp)\n";
     metrics += "# TYPE crawler_start_at gauge\n";
-    metrics += `crawler_runtime_ms ${statistic.crawlerLastStartTimestamp ?? 0}\n`;
+    metrics += `crawler_start_at ${statistic.crawlerLastStartTimestamp ?? 0}\n`;
 
     metrics += "# HELP crawler_runtime_ms 运行时间(ms)\n";
     metrics += "# TYPE crawler_runtime_ms gauge\n";
     metrics += `crawler_runtime_ms ${statistic.crawlerRuntimeMillis ?? 0}\n`;
 
-    metrics += "# HELP request_count 请求统计\n";
-    metrics += "# TYPE request_count counter\n";
-    metrics += `request_count{type="finished",} ${statistic.requestsFinished ?? 0}\n`;
-    metrics += `request_count{type="failed",} ${statistic.requestsFailed ?? 0}\n`;
-    metrics += `request_count{type="retries",} ${statistic.requestsRetries ?? 0}\n`;
+    metrics += "# HELP crawler_request_count 请求统计\n";
+    metrics += "# TYPE crawler_request_count counter\n";
+    metrics += `crawler_request_count{type="finished",} ${statistic.requestsFinished ?? 0}\n`;
+    metrics += `crawler_request_count{type="failed",} ${statistic.requestsFailed ?? 0}\n`;
+    metrics += `crawler_request_count{type="retries",} ${statistic.requestsRetries ?? 0}\n`;
 
-    metrics += "# HELP request_count_pre_minute 每分钟请求数\n";
-    metrics += "# TYPE request_count_pre_minute gauge\n";
-    metrics += `request_count{type="finished",} ${statistic.requestsFinishedPerMinute ?? 0}\n`;
-    metrics += `request_count{type="failed",} ${statistic.requestsFinishedPerMinute ?? 0}\n`;
+    metrics += "# HELP crawler_request_count_pre_minute 每分钟请求数\n";
+    metrics += "# TYPE crawler_request_count_pre_minute gauge\n";
+    metrics += `crawler_request_count_pre_minute{type="finished",} ${statistic.requestsFinishedPerMinute ?? 0}\n`;
+    metrics += `crawler_request_count_pre_minute{type="failed",} ${statistic.requestsFinishedPerMinute ?? 0}\n`;
 
-    metrics += "# HELP request_total_duration_ms 总请求时间(ms)\n";
-    metrics += "# TYPE request_total_duration_ms counter\n";
-    metrics += `request_total_duration_ms ${statistic.requestTotalDurationMillis ?? 0}\n`;
+    metrics += "# HELP crawler_request_total_duration_ms 总请求时间(ms)\n";
+    metrics += "# TYPE crawler_request_total_duration_ms counter\n";
+    metrics += `crawler_request_total_duration_ms ${statistic.requestTotalDurationMillis ?? 0}\n`;
 
-    metrics += "# HELP request_min_duration_ms 最大请求时间(ms)\n";
-    metrics += "# TYPE request_min_duration_ms gauge\n";
-    metrics += `request_min_duration_ms ${statistic.requestMinDurationMillis ?? 0}\n`;
+    metrics += "# HELP crawler_request_min_duration_ms 最大请求时间(ms)\n";
+    metrics += "# TYPE crawler_request_min_duration_ms gauge\n";
+    metrics += `crawler_request_min_duration_ms ${statistic.requestMinDurationMillis ?? 0}\n`;
 
-    metrics += "# HELP request_max_duration_ms 最小请求时间(ms)\n";
-    metrics += "# TYPE request_max_duration_ms gauge\n";
-    metrics += `request_max_duration_ms ${statistic.requestMaxDurationMillis ?? 0}\n`;
+    metrics += "# HELP crawler_request_max_duration_ms 最小请求时间(ms)\n";
+    metrics += "# TYPE crawler_request_max_duration_ms gauge\n";
+    metrics += `crawler_request_max_duration_ms ${statistic.requestMaxDurationMillis ?? 0}\n`;
 
-    metrics += "# HELP request_avg_duration_ms 平均请求时间(ms)\n";
-    metrics += "# TYPE request_avg_duration_ms gauge\n";
-    metrics += `request_avg_duration_ms{type="finished",} ${statistic.requestAvgFinishedDurationMillis ?? -1}\n`;
-    metrics += `request_avg_duration_ms{type="failed",} ${statistic.requestAvgFailedDurationMillis ?? -1}\n`;
+    metrics += "# HELP crawler_request_avg_duration_ms 平均请求时间(ms)\n";
+    metrics += "# TYPE crawler_request_avg_duration_ms gauge\n";
+    metrics += `crawler_request_avg_duration_ms{type="finished",} ${statistic.requestAvgFinishedDurationMillis ?? 0}\n`;
+    metrics += `crawler_request_avg_duration_ms{type="failed",} ${statistic.requestAvgFailedDurationMillis ?? 0}\n`;
   }
+  metrics = metrics.slice(0, -1);
   return metrics;
 };
 
